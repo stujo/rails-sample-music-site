46d8f07d65575309aeb02bc6d845fd74e1bbb6a2
Stujo <email": null>
2016-04-29 11:24:19 -0700

    Logout Test

---
+++ spec/features/user_logs_out_spec.rb
@@ -0,0 +1,17 @@
+feature 'User logs out' do
+
+  scenario 'they are redirected to the welcome page' do
+    visit signup_path
+
+    fill_in 'user[name]', with: 'Mary'
+    fill_in 'user[password]', with: 'MaryMaryPass'
+    fill_in 'user[password_confirmation]', with: 'MaryMaryPass'
+
+    click_button 'Register'
+
+    click_link 'Logout'
+
+    expect(current_path).to eq "/"
+    expect(page.body).to include "Sign Up"
+  end
+end
